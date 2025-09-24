f8398c4a8117b53823f38354a1fb1facea6d225c
Rodrigo <rotavares@proton.me>
2025-09-24 16:56:53 -0300

    exercícios 1 e 2 do capítulo 5 finalizados

---
+++ Cap5/5_1.py
@@ -0,0 +1,7 @@
+soma = qtde = 0
+A = 1
+while A != 0:
+    A = int(input("Digite X: "))
+    soma = soma + A
+    qtde = qtde + 1
+print(f'Soma do valores = {soma}')
---
+++ Cap5/5_2.py
@@ -0,0 +1,7 @@
+N = int(input('Digite N: '))
+i = 1
+while i < N:
+    if i % 7 == 0:
+        print(i)
+    i += 1
+print('Fim do programa')
